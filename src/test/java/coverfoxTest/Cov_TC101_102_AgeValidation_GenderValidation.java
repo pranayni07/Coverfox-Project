f811d2f4b5eeaa75168cd1517c365f7e39f18f33
pranayni07 <pranayni07@gmail.com>
2024-10-15 13:48:16 +0530

    [Pranay]New feature added

---
+++ src/test/java/coverfoxTest/Cov_TC101_102_AgeValidation_GenderValidation.java
@@ -0,0 +1,11 @@
+package coverfoxTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Cov_TC101_102_AgeValidation_GenderValidation {
+  @Test
+  public void TC_101_A() {
+	  Reporter.log(null);
+  }
+}
